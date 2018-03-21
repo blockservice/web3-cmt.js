2328dc7798c8d4435b23bba271d09d6c7247b93b
wangshishuo <wangshishuo@gmail.com>
2018-03-21 11:51:36 +0800

    not use mocha in stress test

--- test/stress.test.js
+++ test/stress.test.js
@@ -1,38 +1,23 @@
 const async = require("async")
-const expect = require("chai").expect
 const Web3 = require("../src/index")
 const { Settings, Wallet } = require("./constants")
 
-let web3
+let web3 = new Web3(new Web3.providers.HttpProvider(Settings.Provider))
+web3.personal.unlockAccount(Wallet[0].Addr, Wallet[0].Password)
 
-const transfer = (f, t, v) => {
-  // send
-  let payload = {
-    from: f,
-    to: t,
-    value: v
-  }
-  async.times(1000, (n) => {
-    payload.value = n + 1
-    web3.cmt.sendTransaction(payload, (err, result) => {
-      if (!err) {
-        console.log(result)
-      } else {
-        console.log(err)
-      }
-    })
-  })
+// send
+let payload = {
+  from: Wallet[0].Addr,
+  to: Wallet[1].Addr,
+  value: 0
 }
-
-describe("Transaction Test", function() {
-  before(function() {
-    web3 = new Web3(new Web3.providers.HttpProvider(Settings.Provider))
-  })
-
-  describe("Free CMT TX from A to B to C to D", function() {
-    it("From A to B", function() {
-      web3.personal.unlockAccount(Wallet[0].Addr, Wallet[0].Password)
-      transfer(Wallet[0].Addr, Wallet[1].Addr, 1)
-    })
+async.times(2, (n) => {
+  payload.value = n + 1
+  web3.cmt.sendTransaction(payload, (err, result) => {
+    if (!err) {
+      console.log(result)
+    } else {
+      console.log(err)
+    }
   })
 })
